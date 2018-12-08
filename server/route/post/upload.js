67cb4cb90c47a3470b6566a90395f6c299a7f4b3
pangzong <pangzong@wsncm.com>
2018-12-08 10:01:34 +0800

    'updates'

--- server/route/post/upload.js
+++ server/route/post/upload.js
@@ -3,18 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const uploadsPath = path.resolve('/root/statics/img/uploads');
 
-const form = new multiparty.Form({
-  autoFiles: true,
-  uploadDir: path.resolve('./'),
-  maxFieldsSize: 2 * 1024 * 1024
-})
-
 module.exports = (req, res) => {
 
+  const form = new multiparty.Form({
+    autoFiles: true,
+    uploadDir: uploadsPath,
+    maxFieldsSize: 2 * 1024 * 1024
+  })
+
   form.parse(req, function(err, fields, files) {
-    console.log(err)
-    console.error(files)
-    console.log('----------')
     if (err) {
       res.send({
         message: '上传出错',
@@ -23,26 +20,26 @@ module.exports = (req, res) => {
       return 
     }
 
-    // if (files.file[0].size > form.maxFieldsSize) {
-    //   res.send({
-    //     message: '文件大小超过200K',
-    //     status: 500
-    //   })
+    if (files.file[0].size > form.maxFieldsSize) {
+      res.send({
+        message: '文件大小超过200K',
+        status: 500
+      })
 
-    //   // 删除文件
-    //   fs.unlink(files.file[0].path, err => {
-    //     console.log(err)
-    //   })
-    //   return;
-    // }
+      // 删除文件
+      fs.unlink(files.file[0].path, err => {
+        console.log(err)
+      })
+      return;
+    }
     
-    // let path = files.file[0].path;
-    // let src = path.replace('/root', 'http://www.parsonz.xyz')
+    let path = files.file[0].path;
+    let src = path.replace('/root', 'http://www.parsonz.xyz')
 
     res.send({
       status: 200,
       message: '暂存成功',
-      src: 'asda'
+      src
     })
   })
 
