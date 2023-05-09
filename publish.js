067ea4abe457716fc23cbee5a0ff04cf8782acca
parsonz <18001171283@163.com>
2023-05-10 00:27:47 +0800

    fix

--- publish.js
+++ publish.js
@@ -1,48 +1,54 @@
 const { exec } = require('child_process');
 
 const publish = async () => {
-  console.log(`git拉取...等待中...`)
-  await exec('git checkout .')
-  await exec('git checkout master')
-  await exec('git pull origin master')
-  
-  console.log(`remove dist...等待中...`)
+  await exec('git checkout master', (err) => {
+    if (!err) {
+      return true
+    }
+  })
+  await exec('git pull origin master', err => {
+    if (!err) {
+      return true
+    }
+  })
+  await console.log('git拉取完成')
+
   await exec('rm -rf dist', (err, stdout, stderr) => {
     if (!err) {
       return true
     }
   })
+  await console.log('remove list完成')
 
-  console.log(`remove node_modules...等待中...`)
   await exec('rm -rf node_modules', (err, stdout, stderr) => {
     if (!err) {
       return true
     }
   })
+  await console.log('remove Modules完成')
 
-  console.log(`npm install...等待中...`)
   await exec('npm install', (err, stdout, stderr) => {
     if (!err) {
       return true
     }
   })
-  
-  console.log(`npm build...等待中...`)
+  await console.log('install完成')
+
   await exec('npm run build', (err, stdout, stderr) => {
     if (!err) {
       return true
     }
   })
-  
-  console.log(`copy dist...等待中...`)
+  await console.log('build完成')
+
   await exec('rm -rf /usr/share/nginx/html/dist')
   await exec('cp -r /root/parsonz/vue-element-ui/dist /usr/share/nginx/html', (err, stdout, stderr) => {
     if (!err) {
       return true
     }
   })
-  
-  console.log(`nginx重启...等待中...`)
+
+  await console.log(`nginx重启...等待中...`)
   await exec('systemctl restart nginx')
 
   return {
